6bae4228a7b5bd238f8a08d5608f164e4c4e7138
heon <dlgjsdl26@gmail.com>
2020-04-21 17:18:17 +0900

    IF를 통한 심리테스트만들기

---
+++ SimLiTest.java
@@ -0,0 +1,214 @@
+package c_statement;
+
+import java.util.Scanner;
+
+public class SimLiTest {
+
+	public static void main(String[] args) {
+		
+	Scanner s = new Scanner(System.in);
+	
+	System.out.println("나는 금사빠다.(Y/N)");
+	String yn = s.nextLine();
+	
+	if(yn.equals("Y")){
+		System.out.println("연애할때 끌려다니는 타입이다.");
+		yn = s.nextLine();
+	
+	}else if(yn.equals("N")){
+		System.out.println("감정기복이 크지 않다.");
+		yn = s.nextLine();
+		
+		}
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*		 System.out.print("나는 금사빠다? Yes or No >");
+		String answer1 = s.nextLine();
+
+		String test1 = 
+				
+//				answer1 = s.nextLine();
+*/		
+	/*System.out.print("나는 금사빠다? Yes or No >");
+		 String answer = s.nextLine();
+
+	outer : for(int i = 1; i<100; i++){ 
+			
+			if(i == 1){
+				if(answer.equals("Yes")){
+					System.out.print("연애할 때 끌려다니는 타입이다? Yes or No >");
+					 String answer1 = s.nextLine();
+					 i++;
+				}
+				else if(answer.equals("NO")){
+					System.out.print("감정기복이 크지않다? Yes or No >");
+					 String answer1 = s.nextLine();
+					 i += 10;
+				}
+			
+			}
+			if(i == 2){
+				if(answer.equals("Yes")){
+					System.out.print("감정표현이 솔직한 편이다? Yes or No >");
+					String answer1 = s.nextLine();
+					i++;
+			}
+			else if(answer.equals("NO")){
+				System.out.print("데이트코스는 미리 짜는게 편하다? Yes or No >");
+				 String answer1 = s.nextLine();
+				 i += 10;
+			}
+				
+				
+				
+				if(i == 11){
+					if(answer.equals("Yes")){
+						System.out.print("연락이 없어도 믿고 기다리는 편이다? Yes or No >");
+						String answer1 = s.nextLine();
+						i++;
+				}
+				else if(answer.equals("NO")){
+					System.out.print("감정표현에 솔직한 편이다? Yes or No >");
+					 String answer1 = s.nextLine();
+					 i += 10;
+			}
+					
+					
+					
+			if(i == 3){
+				if(answer.equals("Yes")){
+					System.out.print("이성친구는 존재할 수 없다? Yes or No >");
+					String answer1 = s.nextLine();
+					i++;
+			}
+			else if(answer.equals("NO")){
+				System.out.print("활동적인 데이트코스가 좋다? Yes or No >");
+				 String answer1 = s.nextLine();
+				 i += 10;
+			
+	}
+	}
+}*/
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*		
+//	do{	
+	outer : if(answer1.equals("Yes")){
+		System.out.print("연애할 때 끌려다니는 타입이다? Yes or NO >");
+		String answer2 = s.nextLine();
+		
+		if(answer2.equals("Yes")){
+			System.out.print("감정표현에 솔직한 편이다? Yes or NO >");
+			String answer3 = s.nextLine();
+			
+			if(answer3.equals("Yes")){
+				System.out.print("이성친구는 존재할 수 없다? Yes or NO >");
+				String answer4 = s.nextLine();
+				
+				if(answer4.equals("Yes")){
+					System.out.print("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다? Yes or NO >");
+					String answer5 = s.nextLine();
+					
+					if(answer5.equals("Yes")){
+						System.out.print("타입 : D , 무조건 잘해주고, 무조건 맞춰주는 다퍼주는 연애를 하는타입!");
+						break outer;
+					}else if(answer1.equals("NO")){
+						System.out.print("감정기복이 크지않다? Yes or NO >");
+						String ans2 = s.nextLine();
+					}else System.out.println("잘못된 대답입니다. Yes or No 중에 선택해주세요.");
+						System.out.print("나는 금사빠다? Yes or No >");
+						 answer1 = s.nextLine();
+				}else if(answer4.equals("NO")){
+					System.out.print("타입 : B , 구속을 하는것도 받는것도 싫은 자유로운 연애타입 입니다.");
+					break outer;
+				}else System.out.println("잘못된 대답입니다. Yes or No 중에 선택해주세요.");
+					System.out.print("이성친구는 존재할 수 없다? Yes or No >");
+					 answer4 = s.nextLine();
+					 
+					 
+					 
+			}else if(answer3.equals("NO")){
+				System.out.print("활동적인 데이트가 좋다? Yes or NO >");
+				String ans4= s.nextLine();
+			}else System.out.println("잘못된 대답입니다. Yes or No 중에 선택해주세요.");
+				System.out.print("감정표현이 솔직한 편이다? Yes or No >");
+				 answer3 = s.nextLine();
+				 
+				 
+		}else if(answer2.equals("NO")){
+			System.out.print("데이트 코스는 미리 짜는게 편하다? Yes or NO >");
+			String ans3 = s.nextLine();
+		}else System.out.println("잘못된 대답입니다. Yes or No 중에 선택해주세요.");
+			System.out.print("연애할 때 끌려다니는 타입이다? Yes or No >");
+			 answer2 = s.nextLine();
+			 
+			 
+			 
+			 
+			 
+			 
+//----------------------------------------------------------------------------------			 
+			 
+			 
+	}else if(answer1.equals("NO")){
+		System.out.print("감정기복이 크지않다? Yes or NO >");
+		String ans2 = s.nextLine();
+	}else System.out.println("잘못된 대답입니다. Yes or No 중에 선택해주세요.");
+		System.out.print("나는 금사빠다? Yes or No >");
+		 answer1 = s.nextLine();
+//	}while(answer1 != "Yes" && answer1 != "NO");
+	*/
+		
+	
+	
+	
+	
+
+
