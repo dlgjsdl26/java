434ebdf396f5589b6b6445ff5c3c89197f313584
heon <dlgjsdl26@gmail.com>
2020-04-29 17:16:43 +0900

    D

---
+++ e_oop/Calculator.java
@@ -0,0 +1,25 @@
+package e_oop;
+
+public class Calculator {
+
+	// +
+	double sum(double a, double b){
+		return a + b;
+	}
+	// -
+	double min(double a, double b){
+		return a - b;
+	}
+	// /
+	double nun(double a, double b){
+		return a / b;
+	}
+	// *
+	double gob(double a, double b){
+		return a * b;
+	}
+	// %
+	double nam(double a, double b){
+		return a % b;
+	}
+}
